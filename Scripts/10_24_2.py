93db57b4d9c19e0c8467a56856eba4c599ad8325
cornelld <cornelld@astrion.us>
2025-10-06 08:55:23 -0500

    Completed 10_24_1 and close to finished on 2

--- Scripts/10_24_2.py
+++ Scripts/10_24_2.py
@@ -1,7 +1,7 @@
 import pathlib
 import numpy
 import copy
-f = open(pathlib.Path(r"C:\\Users\cornelld\Documents\Advent-2024\Inputs\input_10_24.txt"), 'r')
+f = open(pathlib.Path(r"C:\\Users\cornelld\Documents\Advent-2024\Inputs\input_10_24_test.txt"), 'r')
 lines = []
 totals = 0
 while True:
@@ -18,10 +18,10 @@ def find_path(x,y, counter, dir):
         if x>0 and lines[x - 1][y] == counter and dir==4:
             stack.append((x, y, counter, 3))
             x -= 1
-        elif y<=38 and lines[x][y + 1] == counter and dir>=3:
+        elif y<=6 and lines[x][y + 1] == counter and dir>=3:
             stack.append((x, y, counter,2))
             y += 1
-        elif x<=38 and lines[x + 1][y] == counter and dir>=2:
+        elif x<=6 and lines[x + 1][y] == counter and dir>=2:
             stack.append((x, y, counter,1))
             x += 1
         elif y>=0 and lines[x][y - 1] == counter and dir>=1:
@@ -34,18 +34,20 @@ def find_path(x,y, counter, dir):
                 return
         if counter == 9:
             global totals
-            if (x,y) not in nines:
-                totals += 1
-                nines.append((x,y))
+            nines.append((x, y))
+            totals += 1
+
+
         counter += 1
         dir = 4
 
 
-for i in range(0,40):
-    for j in range(0,40):
+for i in range(0,8):
+    for j in range(0,8):
         if lines[i][j]==0:
             counter = 1
             dir = 4
             nines = []
             find_path(i,j,counter,dir)
             print(totals)
+            totals = 0

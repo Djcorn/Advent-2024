5c52157eb8b6b6b3da6e78a701cac8f1f64bb7a2
cornelld <cornelld@astrion.us>
2025-10-07 11:18:39 -0500

    Completed 11_24_1 and waiting on result from 11_24_2

---
+++ Scripts/11_24_1-2.py
@@ -0,0 +1,25 @@
+import pathlib
+import numpy
+import copy
+f = open(pathlib.Path(r"C:\\Users\cornelld\Documents\Advent-2024\Inputs\input_11_24.txt"), 'r')
+line = f.readline()
+lines = line.split(" ")
+for i in range(0,75):
+    j = 0
+    while j < len(lines):
+        if int(lines[j]) == 0:
+            lines[j] = "1"
+        elif len(lines[j])%2==0:
+            mid_index = int(len(lines[j])/2)
+            front = lines[j][:mid_index]
+            back = lines[j][mid_index:]
+            lines[j] = front
+            lines.insert(j+1,back.lstrip('0'))
+            if lines[j+1] == '':
+                lines[j + 1] = '0'
+            j+=1
+        else:
+            lines[j] = str(int(lines[j])*2024)
+        j+=1
+#print(lines)
+print(len(lines))
